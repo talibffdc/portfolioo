7e5f301fa5296b729b4d8b18e0d75fcd2783a433
talib.cpp <talibffdc@gmail.com>
2023-12-05 19:10:34 +0530

    ..

--- src/components/About.js
+++ src/components/About.js
@@ -77,6 +77,7 @@ import React from "react";
 import "./aboutstyle.css";
 import imgseparator from "./images/separatorBlack 1.png";
 import Responsive from "./Responsive";
+import designimgg from "./images/designimgg.png"
 
 export default function About() {
   return (
@@ -108,6 +109,7 @@ export default function About() {
           <div className="both">
             <div className="design">
               <h2 className="designtext">DESIGN</h2>
+              
               <p className="abouttextsize">
                 I can design the site based on your needs and suggestions. I can
                 also design the site from scratch and consult you during the job.
--- src/components/Responsive.js
+++ src/components/Responsive.js
@@ -8,18 +8,17 @@ import linkedin from "./images/linkedinrespon.png";
 export default function Responsive() {
   return (
     <>
-      <div>
-        <div className="firstpagerespon"></div>
-        <img className="responimg" src={responfirst} />
-        <div className="text-overlay">my name is Talib</div>
-        <h2 className="developertext">I'M A DEVELOPER</h2>
-        <img className=" separatorrepon " src={separator} />
-        <div className=" threerespon ">
-          <img className="instagramimg" src={instagram} />
-          <img className="twitterimg" src={twitter} />
-          <img className="linkedinimg" src={linkedin} />
-        </div>
+      <img className="responimg" src={responfirst} />
+      <div className="text-overlay">my name is Talib</div>
+      <h2 className="developertext">I'M A DEVELOPER</h2>
+      <img className=" separatorrepon " src={separator} />
+
+      <div className=" threerespon ">
+        <img className="instagramimg" src={instagram} />
+        <img className="twitterimg" src={twitter} />
+        <img className="linkedinimg" src={linkedin} />
       </div>
+      
     </>
   );
 }
